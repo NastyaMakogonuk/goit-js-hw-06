028525c960a3f056779550513147d8778bf933f7
NastyaMakogonuk <111354954+NastyaMakogonuk@users.noreply.github.com>
2023-03-13 18:03:09 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,31 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector("ul.gallery");
+
+const galleryImages = images.forEach(event => {
+  gallery.insertAdjacentHTML("afterbegin", `<li class="image">
+  <img src="${event.url}" alt="${event.alt}" width="600" />
+  </li>`);
+});
+
+console.log(galleryImages);
+
+gallery.style.display = "flex";
+gallery.style.listStyle = "none";
+gallery.style.flexDirection ="column";
+gallery.style.justifyContent ="center";
+gallery.style.alignItems ="center";
+gallery.style.gap ="50px";
+
+const galleryPicture = document.querySelectorAll("img");
+
+galleryPicture.forEach(event =>{
+  event.classList.add = "picture";
+  event.style.borderStyle = "solid";
+  event.style.borderWidth = "15";
+  event.style.borderColor = "red";
+  event.style.borderRadius = "50%";
+
+});
