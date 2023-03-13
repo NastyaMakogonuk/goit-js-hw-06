eb8ecedebcd1391348e955176391c18a77cde7a5
NastyaMakogonuk <111354954+NastyaMakogonuk@users.noreply.github.com>
2023-03-13 18:05:36 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("input#font-size-control");
+const text = document.querySelector("span#text");
+
+input.addEventListener("input", (e) => {
+    text.style.fontSize = `${e.currentTarget.value}px`;
+});
