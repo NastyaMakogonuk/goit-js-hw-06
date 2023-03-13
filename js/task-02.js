f42d800337f038fdff850f56dd0e7cc9cc385127
NastyaMakogonuk <111354954+NastyaMakogonuk@users.noreply.github.com>
2023-03-13 18:01:05 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("ul#ingredients");
+
+const ingredientsItems = ingredients.map( event => {
+  const listItem = document.createElement("li");
+  listItem.textContent = event;
+  listItem.classList = "item";
+  return listItem;
+});
+
+ingredientsList.append(...ingredientsItems);
+
+console.log(ingredientsItems);
